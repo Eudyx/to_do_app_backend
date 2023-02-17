029ddf9a212ed75d23e7f23414bd4b29a6e95c06
Eudys Collado <eudys1.42@gmail.com>
2023-02-16 23:32:42 -0400

    Login created

---
+++ controllers/authController.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcrypt');
+const User = require('../model/User');
+const jwt = require('jsonwebtoken');
+
+const handleLogin = async (req, res) => {
+    const { email, pwd } = req.body;
+
+    if(!email || !pwd) return res.status(400).json({ "message": "Email and password are required" });
+
+    const foundUser = await User.findOne({ email }).exec();
+    
+    if(!foundUser) return res.status(401);
+
+    const match = bcrypt.compare(pwd, foundUser.password);
+    if(match) {
+        const accessToken =  jwt.sign(
+            {
+                "username": foundUser.username
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {expiresIn: '60s'}
+        );
+        
+        const refreshToken =  jwt.sign(
+            {
+                "username": foundUser.username
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            {expiresIn: '1d'}
+        );
+
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+
+        console.log(result);
+
+        res.cookie('jwt', { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+
+        res.json({ accessToken });
+
+    }else{
+        res.sendStaus(401);
+    }
+
+}
+
+module.exports = {handleLogin};
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const cookieParser = require('cookie-parser');
 
 
 const connectDB = require('./config/dbConn');
+const { default: mongoose } = require('mongoose');
 const PORT = process.env.PORT || 3500;
 
 // Connect to MongoDB
@@ -18,9 +20,13 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use('/', require('./routes/root'));
 
 app.use('/register', require('./routes/register'));
+
+app.use('/login', require('./routes/auth'));
 
 app.all('*', (req, res) => {
     res.status(400);
@@ -33,4 +39,7 @@ app.all('*', (req, res) => {
     }
 });
 
-app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
+});
